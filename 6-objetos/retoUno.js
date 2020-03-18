3ae94fbf3e7eaf1e63a19c5be99f97b5dbe552a8
Derly <derlyvivianamurcia@gmail.com>
2020-03-18 12:19:41 -0500

    Trabajo virtual academia geek

---
+++ 6-objetos/retoUno.js
@@ -0,0 +1,17 @@
+//EJERCICIO 1
+//Bio de Geek Girls
+//geekGirl1
+const geekGirl1 = {};
+geekGirl1.name = 'Susana';
+geekGirl1.age = 34;
+geekGirl1.profession = 'periodista';
+
+console.log('Mi nombres es ' + geekGirl1.name + ', tengo ' + geekGirl1.age + ' años y soy ' + geekGirl1.profession);
+
+//geekGirl2
+const geekGirl2 = {};
+geekGirl2.name = 'Rocío';
+geekGirl2.age = 25;
+geekGirl2.profession = 'actriz';
+
+console.log(geekGirl2.name + ', ' + geekGirl2.age + ' años, ' + geekGirl2.profession);
